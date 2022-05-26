1adc0b060388abb50d4d8a56d46596b708df50d4
abc20210708 <87113813+abc20210708@users.noreply.github.com>
2022-05-26 17:56:59 +0900

    null 값 해결하기(3)

--- src/main/java/com/example/shop/cart/repository/CartMapper.java
+++ src/main/java/com/example/shop/cart/repository/CartMapper.java
@@ -1,11 +1,10 @@
 package com.example.shop.cart.repository;
 
 import com.example.shop.cart.domain.Cart;
-import com.example.shop.cart.dto.ModCart;
 import com.example.shop.product.domain.Product;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.List;
 
@@ -31,7 +30,7 @@ public interface CartMapper {
     int sumTotal(String csId);
 
     //장바구니 동일한 상품 확인
-    Integer countCart(@Param("csId") String csId, @Param("prCode") int prCode) throws Exception;
+    Integer countCart(@Param("csId") String csId, @Param("prCode") int prCode);
 
     //장바구니 상품수량 변경
     void updateCart(Cart cart);
